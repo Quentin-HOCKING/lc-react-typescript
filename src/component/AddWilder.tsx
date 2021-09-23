f8a8c5a4a9451cfb4a171c899306a6623c864bc1
Quentin HOCKING <hocking.quentin@gmail.com>
2021-09-23 11:51:52 +0200

    Update addwilders skill

--- src/component/AddWilder.tsx
+++ src/component/AddWilder.tsx
@@ -4,9 +4,9 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { WilderProps } from './Wilder';
 
-const AddWilder = ():JSX.Element => {
+const AddWilder = (): JSX.Element => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = async (data:WilderProps) => {
+  const onSubmit = async (data: WilderProps) => {
     try {
       const result = await axios.post(
         'http://localhost:5000/api/wilders',
@@ -19,16 +19,28 @@ const AddWilder = ():JSX.Element => {
   };
   return (
   <form onSubmit={handleSubmit(onSubmit)}>
-    <label htmlFor='idName'>First Name</label>
-    <input id='idName'{...register('firstName')} />
-    <label htmlFor='idGender'>Gender Selection</label>
-    <select {...register('gender')}>
-      <option value="female">female</option>
-      <option value="male">male</option>
-      <option value="other">other</option>
-    </select>
+    <label htmlFor='idName'>Wilder&apos;s Name</label>
+    <input id='idName'{...register('name')} />
+    <label htmlFor='idCity'>Wilder&apos;s City</label>
+    <input id='idCity'{...register('city')} />
+    <label htmlFor='idCity'>Wilder&apos;s Skill</label>
+    <input id='idSkill'{...register('skills.title')} />
+    <label htmlFor='idVote'>Wilder&apos;s votes</label>
+    <input id='idSkill'{...register('skills.votes', {min: 0})} type="number" />
+  
     <input type="submit" />
   </form>
+
+/* <form onSubmit={handleSubmit(onSubmit)}>
+<input type="text" placeholder="First name" {...register("First name", {required: true, maxLength: 80})} />
+<input type="text" placeholder="Wilder's City" {...register("Wilder's City", {required: true, maxLength: 100})} />
+<input type="text" placeholder="Wilder's First Skill" {...register("Wilder's First Skill", {required: true})} />
+<input type="number" placeholder="Wilder's first skill votes" {...register("Wilder's first skill votes", {required: true, min: 0})} />
+<input type="text" placeholder="Wilder's Second Skill" {...register("Wilder's Second Skill", {})} />
+<input type="number" placeholder="Wilder's Second skill votes" {...register("Wilder's Second skill votes", {, min: 0})} />
+
+<input type="submit" />
+</form> */
   );
 };
 
